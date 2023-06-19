5174414ceb5d938473b30f40de2709759d3568de
Nar Cuenca <narc.ph@gmail.com>
2023-06-19 15:42:39 +0800

    task: add autohide code

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,55 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { TextEditorSelectionChangeKind } from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "declutter" is now active!');
+  let timerId: NodeJS.Timeout;
+  const timeoutDelay: number =
+    vscode.workspace.getConfiguration("declutter").delay;
+  const isAutoHideSidebar: boolean =
+    vscode.workspace.getConfiguration("declutter").autoHideSidebar;
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "declutter.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from declutter!");
-    }
+  vscode.window.showInformationMessage(
+    `Current timer delay is: ${timeoutDelay}`
+  );
+  vscode.window.showInformationMessage(
+    `Autohide sidebar: ${isAutoHideSidebar}`
   );
 
-  context.subscriptions.push(disposable);
+  function triggerAutohide() {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      vscode.commands.executeCommand("workbench.action.closeSidebar");
+    }, timeoutDelay);
+  }
+
+  if (isAutoHideSidebar) {
+    // User selected something on the text editor
+    vscode.window.onDidChangeTextEditorSelection((evt) => {
+      if (!!evt && evt.kind === TextEditorSelectionChangeKind.Mouse) {
+        triggerAutohide();
+      }
+    });
+
+    // User changed tabs
+    vscode.window.onDidChangeActiveTextEditor((evt) => {
+      console.log(evt);
+      if (!!evt) {
+        triggerAutohide();
+      }
+    });
+  }
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "declutter.updateAutoHideSidebar",
+      async () => {
+        const config = vscode.workspace.getConfiguration("declutter");
+
+        await config.update("autoHideSidebar", !config.autoHideSidebar, false);
+      }
+    )
+  );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
